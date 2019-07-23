58c39aa91b762682d3e77e1c466eebe7c9095c8f
maxmargolin <margolinmax@gmail.com>
2019-07-23 21:39:56 +0300

    extractor - get features from sounds

--- python3/extractor.py
+++ python3/extractor.py
@@ -1,45 +1,39 @@
 __author__ = "Max Margolin, margolinmax@gmail.com"
-#pay no attention to this yet
+# pay no attention to this yet
 
-# Beat tracking example
-from __future__ import print_function
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename1 = "og.mp3"
-filename = "lyricsl.mp3"
-# 2. Load the audio as a waveform `y`
-#    Store the sampling rate as `sr`
-y, sr = librosa.load(filename)
-claim = [30]
-# 3. Run the default beat tracker
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
-
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
-
-
+filename = "og.mp3"
 
 
 def extract(current_time, wave, sr, time_multiples):
-    print(len(wave)/sr)
-    current = current_time*sr
+    """
+
+    :param current_time:
+    :param wave:
+    :param sr:
+    :param time_multiples:
+    :return:
+    """
+    current = current_time * sr
     arr = []
-    for i in range(-10,10):
+    for i in range(-10, 10):
         start = current + sr * i * time_multiples
         finish = current + sr * (i + 1) * time_multiples
         arr.append(np.average(np.abs(wave[start:finish])))
     return arr
-e = extract(claim[0], y, sr, 1)
 
 
-plt.figure()
-#plt.subplot(3, 1, 1)
-#librosa.display.waveplot(y, sr=sr)
+def get_nearby(filename, claims):
+    y, sr = librosa.load(filename)
+    e = extract(claims[0], y, sr, 1)
+    return e
 
-plt.plot(range(-10,10),e)
-print (claim[0])
-print(len(y)/sr)
 
-plt.show()
+def get_nearby_up_down(filename, claims):
+    a = get_nearby(filename, claims)
+    diff = np.diff(a)
+    return diff > 0
