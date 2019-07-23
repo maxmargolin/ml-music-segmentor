19d35ffafafd4c4e6856368edc36fc0a88ccd3d8
maxmargolin <margolinmax@gmail.com>
2019-07-23 21:41:03 +0300

    compare - get features from comparing 2 sound files

--- python3/compare.py
+++ python3/compare.py
@@ -6,8 +6,6 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename1 = "og.mp3"
-filename2 = "lyrical.mp3"
 claim = [30]
 UNIT_WIDTH = 5
 
@@ -30,7 +28,7 @@ def get_sound_wave_averages(filename, time_multiples):
     get the sound wave values, averaged for every %time_multiples% time period
     :param filename: name of sound file
     :param time_multiples: time unit in second to average
-    :return: array of averages over every @time_multiples seconds
+    :return: array of averages over every time_multiples seconds
 
     note
     """
@@ -42,7 +40,7 @@ def get_sound_wave_averages(filename, time_multiples):
     return averages
 
 
-def find_peak_correlation(main_sound_wave, secondary_sound_wave, unit_width=UNIT_WIDTH):
+def get_peak_correlation(main_sound_wave, secondary_sound_wave, unit_width=UNIT_WIDTH):
     """
     find the best offset for peak correlation.
     by returning the maximum of the correlation by offset array.
@@ -54,7 +52,39 @@ def find_peak_correlation(main_sound_wave, secondary_sound_wave, unit_width=UNIT
     return unit_width * np.argmax(np.correlate(main_sound_wave, secondary_sound_wave))
 
 
-og_wave = get_sound_wave_averages(filename1, UNIT_WIDTH)
-lyrical_wave = get_sound_wave_averages(filename2, UNIT_WIDTH)
-# plot_waves(og_wave,lyrical_wave)
-print("peak correlation @ second ", find_peak_correlation(og_wave, lyrical_wave))
+def get_correlation_array(main_sound_wave, secondary_sound_wave, unit_width=UNIT_WIDTH):
+    """
+    get the correlation by offset
+    :param main_sound_wave: original video sound
+    :param secondary_sound_wave:  lyrical video sound
+    :param unit_width: size of average unit in seconds
+    :return: correlation array
+    """
+    return np.correlate(main_sound_wave, secondary_sound_wave)
+
+
+def get_correlation_features(filename1="og.mp3", filename2="lyrical.mp3", unit_width=UNIT_WIDTH):
+    """
+    get all features from this file
+    :param filename1: og
+    :param filename2:  lyrical
+    :param unit_width:  sound unit to avg
+    :return:
+    """
+    og_wave = get_sound_wave_averages(filename1, unit_width)
+    lyrical_wave = get_sound_wave_averages(filename2, unit_width)
+    return get_correlation_array(og_wave, lyrical_wave), get_peak_correlation(og_wave, lyrical_wave)
+
+
+def get_lengths(filename1="og.mp3", filename2="lyrical.mp3"):
+    """
+    TODO: rethink this 2 sounds in 1 function thing
+    :param filename1: og
+    :param filename2:  lyrical
+    :return:  2 lengths
+    """
+    wave1, rate1 = librosa.load(filename1)
+    wave2, rate2 = librosa.load(filename2)
+    return len(wave1) / rate1, len(wave2) / rate2
+
+
